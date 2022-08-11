4976e6cae03267981a51bc0e7499d5f4ba56f7bc
ANJARASOA Bernardin <bernardinanjarasoa@hotmail.com>
2022-08-11 20:03:08 +0300

    Updates

---
+++ precache-manifest.5677d8dfc6ff079f58b901d4fcba7859.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40cd4047a2887a296392e016b977eda7",
+    "url": "/mada-marketing/index.html"
+  },
+  {
+    "revision": "c8605889bd6ae973c42a",
+    "url": "/mada-marketing/static/css/2.344178ee.chunk.css"
+  },
+  {
+    "revision": "ddde663538fb2d54ad63",
+    "url": "/mada-marketing/static/css/main.d54193e9.chunk.css"
+  },
+  {
+    "revision": "c8605889bd6ae973c42a",
+    "url": "/mada-marketing/static/js/2.89596a1f.chunk.js"
+  },
+  {
+    "revision": "b7cd4ab849b71ffd48da452d762015e6",
+    "url": "/mada-marketing/static/js/2.89596a1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddde663538fb2d54ad63",
+    "url": "/mada-marketing/static/js/main.2402b437.chunk.js"
+  },
+  {
+    "revision": "7327d66dc945defa58ac",
+    "url": "/mada-marketing/static/js/runtime-main.d6b5a23e.js"
+  },
+  {
+    "revision": "85064a97348166467bd7bc2ad80ccdce",
+    "url": "/mada-marketing/static/media/404.85064a97.svg"
+  },
+  {
+    "revision": "a0320da1a9ae65d73f6af7780883975c",
+    "url": "/mada-marketing/static/media/500.a0320da1.svg"
+  },
+  {
+    "revision": "d44e638f1f67903c0851380f173ea95d",
+    "url": "/mada-marketing/static/media/CIS.d44e638f.PNG"
+  },
+  {
+    "revision": "e9f6609295e10cca7a64d98bfbbdbdc1",
+    "url": "/mada-marketing/static/media/VAC.e9f66092.PNG"
+  },
+  {
+    "revision": "6aae84abf0090324eb1daa7eef6bfdd8",
+    "url": "/mada-marketing/static/media/bootstrap-5-logo.6aae84ab.svg"
+  },
+  {
+    "revision": "27c54a3e21700cd1ce6b0358a45e0b6e",
+    "url": "/mada-marketing/static/media/bscall.27c54a3e.png"
+  },
+  {
+    "revision": "a7d2a8d1e14afb27a701459a5957f170",
+    "url": "/mada-marketing/static/media/call.a7d2a8d1.svg"
+  },
+  {
+    "revision": "ed3cd4b507f8209412bc426332f23589",
+    "url": "/mada-marketing/static/media/canada.ed3cd4b5.svg"
+  },
+  {
+    "revision": "8688048f4828689111b424d4ac007f5f",
+    "url": "/mada-marketing/static/media/cvs.8688048f.PNG"
+  },
+  {
+    "revision": "0c9f225e8f69c622f681cf1ed973cc3d",
+    "url": "/mada-marketing/static/media/fa-brands-400.0c9f225e.woff2"
+  },
+  {
+    "revision": "0ce1e868452204695c8ac1c70f693c2d",
+    "url": "/mada-marketing/static/media/fa-brands-400.0ce1e868.woff"
+  },
+  {
+    "revision": "5d10d00980077f67990f73b1961bc46e",
+    "url": "/mada-marketing/static/media/fa-brands-400.5d10d009.svg"
+  },
+  {
+    "revision": "d28c96cabe5302f1b2c90275cd3f4e69",
+    "url": "/mada-marketing/static/media/fa-brands-400.d28c96ca.eot"
+  },
+  {
+    "revision": "dfe5aa4344a2d8a29aec8d83fb3fb14e",
+    "url": "/mada-marketing/static/media/fa-brands-400.dfe5aa43.ttf"
+  },
+  {
+    "revision": "0232fe8b09de812c88e6cc27f57a3a88",
+    "url": "/mada-marketing/static/media/fa-regular-400.0232fe8b.eot"
+  },
+  {
+    "revision": "398be22b2308585b84da8e858e2d7b60",
+    "url": "/mada-marketing/static/media/fa-regular-400.398be22b.ttf"
+  },
+  {
+    "revision": "3cace4a04d941b5981ba32c6ce9afae1",
+    "url": "/mada-marketing/static/media/fa-regular-400.3cace4a0.woff"
+  },
+  {
+    "revision": "847712aaabbeba674afdda86d31cab17",
+    "url": "/mada-marketing/static/media/fa-regular-400.847712aa.woff2"
+  },
+  {
+    "revision": "d0fdc7c20b0b0b1aaf27327547735fa3",
+    "url": "/mada-marketing/static/media/fa-regular-400.d0fdc7c2.svg"
+  },
+  {
+    "revision": "00bb62bc33034479d5e82a0fd1c5efe3",
+    "url": "/mada-marketing/static/media/fa-solid-900.00bb62bc.eot"
+  },
+  {
+    "revision": "2306f642b31b774ba1d8b6b784539e19",
+    "url": "/mada-marketing/static/media/fa-solid-900.2306f642.svg"
+  },
+  {
+    "revision": "260d033f79f75d670fd09d8cf7c8bf8d",
+    "url": "/mada-marketing/static/media/fa-solid-900.260d033f.ttf"
+  },
+  {
+    "revision": "4bfbf7eb4b19d9ff9293eb177b6d0070",
+    "url": "/mada-marketing/static/media/fa-solid-900.4bfbf7eb.woff"
+  },
+  {
+    "revision": "9ae050d1876ac1763eb6afe4264e6d5a",
+    "url": "/mada-marketing/static/media/fa-solid-900.9ae050d1.woff2"
+  },
+  {
+    "revision": "968aaa24eeffdd779e4fc9c27c5cb730",
+    "url": "/mada-marketing/static/media/france.968aaa24.svg"
+  },
+  {
+    "revision": "d810f62137d64a0a58035c80489b7d54",
+    "url": "/mada-marketing/static/media/germany.d810f621.svg"
+  },
+  {
+    "revision": "612e617f5d72975e63e53d793d757785",
+    "url": "/mada-marketing/static/media/italy.612e617f.svg"
+  },
+  {
+    "revision": "1f905d23af144bc76e488f00d516de2e",
+    "url": "/mada-marketing/static/media/japan.1f905d23.svg"
+  },
+  {
+    "revision": "b4c8fb6c1900dc3cd1ce7821866f3a62",
+    "url": "/mada-marketing/static/media/logoCalisto.b4c8fb6c.png"
+  },
+  {
+    "revision": "d2044d47dece62708ba2368492f5b2e7",
+    "url": "/mada-marketing/static/media/mockup-map-presentation.d2044d47.png"
+  },
+  {
+    "revision": "3d0750b3d0c8b67e443685aac8789a42",
+    "url": "/mada-marketing/static/media/mockup-presentation.3d0750b3.png"
+  },
+  {
+    "revision": "94281e63587bfa4447f6ae7292a5fc02",
+    "url": "/mada-marketing/static/media/prc.94281e63.PNG"
+  },
+  {
+    "revision": "9992e0ee061c844b089dd94784cf7e9d",
+    "url": "/mada-marketing/static/media/profile-cover.9992e0ee.jpg"
+  },
+  {
+    "revision": "4d26e151b224eaa88df2742351073c56",
+    "url": "/mada-marketing/static/media/profile-picture-1.4d26e151.jpg"
+  },
+  {
+    "revision": "487133c5319652023f459ea4e3a4720a",
+    "url": "/mada-marketing/static/media/profile-picture-2.487133c5.jpg"
+  },
+  {
+    "revision": "9a250e5ca0df93fd467f77c4d7e3d3d4",
+    "url": "/mada-marketing/static/media/profile-picture-3.9a250e5c.jpg"
+  },
+  {
+    "revision": "ccd796b31c6ca0c778c6065b3b6a20b7",
+    "url": "/mada-marketing/static/media/profile-picture-4.ccd796b3.jpg"
+  },
+  {
+    "revision": "baf7bf753ca889c7db833fb0db47abac",
+    "url": "/mada-marketing/static/media/profile-picture-5.baf7bf75.jpg"
+  },
+  {
+    "revision": "a7d2a8d1e14afb27a701459a5957f170",
+    "url": "/mada-marketing/static/media/react-hero-logo.a7d2a8d1.svg"
+  },
+  {
+    "revision": "a7d2a8d1e14afb27a701459a5957f170",
+    "url": "/mada-marketing/static/media/react-logo-primary.a7d2a8d1.svg"
+  },
+  {
+    "revision": "a7d2a8d1e14afb27a701459a5957f170",
+    "url": "/mada-marketing/static/media/react-logo-transparent.a7d2a8d1.svg"
+  },
+  {
+    "revision": "a7d2a8d1e14afb27a701459a5957f170",
+    "url": "/mada-marketing/static/media/react-logo.a7d2a8d1.svg"
+  },
+  {
+    "revision": "58ee1d5a0a93d00df3f6c12170ebd3ff",
+    "url": "/mada-marketing/static/media/signin.58ee1d5a.svg"
+  },
+  {
+    "revision": "105e5059b0997718976601d17fd2a04b",
+    "url": "/mada-marketing/static/media/themesberg.105e5059.svg"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/mada-marketing/static/media/united-kingdom.e5564902.svg"
+  },
+  {
+    "revision": "36ab476e5e55f496749ee61897a9cfb5",
+    "url": "/mada-marketing/static/media/united-states-of-america.36ab476e.svg"
+  }
+]);
